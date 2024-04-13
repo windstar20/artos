72cd7fbfa718cc2debdc939cf6489a7583463687
james <james@irm.kr>
2024-04-13 12:19:56 +0900

    [feature] Add order for product

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css'
-import './reset.css'
+import "./App.css";
+import "./reset.css";
 import { useState } from "react";
 
 function App() {
-  const [products, setProducts] = useState([
+  const [products] = useState([
     {
       id: "101",
       type: "beverage",
@@ -31,14 +31,102 @@ function App() {
       imageUrl:
         "https://image.istarbucks.co.kr/upload/store/skuimg/2023/04/[9200000004566]_20230407153247174.jpg",
     },
+    {
+      id: "104",
+      type: "beverage",
+      name: "Grapefruit Honey Black Tea",
+      nameKor: "자몽 허니 블랙 티",
+      price: 6500,
+      imageUrl:
+        "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000187]_20210419131229539.jpg",
+    },
+    {
+      id: "105",
+      type: "beverage",
+      name: "Malcha Tiramisu Latte",
+      nameKor: "말차 티라미수 라떼",
+      price: 6500,
+      imageUrl:
+        "https://image.istarbucks.co.kr/upload/store/skuimg/2023/11/[9200000004954]_20231127093740911.jpg",
+    },
+    {
+      id: "106",
+      type: "beverage",
+      name: "Iced Jeju Green Tea",
+      nameKor: "유기농 녹차로 만든 티",
+      price: 6500,
+      imageUrl:
+        "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[400400000094]_20210415230316646.jpg",
+    },
   ]);
 
+  const convertNumType = (number: number) => {
+    return number.toLocaleString("ko-KR", {
+      // style: 'currency',
+      // currency: 'USD',
+      // maximumFractionDigits: 2,
+      // minimumFractionDigits: 2,
+    });
+  };
+
+  const [orderList, setOrderList] = useState([]);
+
+  const handleProductClick = (e, product) => {
+    console.log(
+      "[ App/handleProductClick : 76, james ] product click =",
+      product,
+    );
+    setOrderList((prevState) => {
+      if (prevState.length === 0) {
+        return [
+          ...prevState,
+          {
+            id: product.id,
+            count: 1,
+          },
+        ];
+      } else {
+        const hasProductIdx = orderList.findIndex(
+          (item) => item.id === product.id,
+        );
+        console.log("[ App/ : 90, james ] hasProduct =", hasProductIdx);
+        if (hasProductIdx >= 0) {
+          return prevState.map((item, idx) => {
+            if (idx === hasProductIdx) {
+              return {
+                ...item,
+                count: item.count + 1,
+              };
+            } else {
+              return item;
+            }
+          });
+        } else {
+          return [
+            ...prevState,
+            {
+              id: product.id,
+              count: 1,
+            },
+          ];
+        }
+      }
+    });
+  };
+
   return (
-    <main className={"main"}>
-      <ul className='w-full  bg-white p-2.5 grid gap-y-5'>
+    <main className={"main bg-white"}>
+      <ul className="w-full  bg-white p-2.5 grid grid-cols-3x350 gap-3">
         {products.map((product) => {
           return (
-            <li key={product.id} className={"p-15 px-10 grid grid-cols-[100px_1fr] gap-x-10 items-center"}>
+            <li
+              key={product.id}
+              data-id={product.id}
+              className={
+                "py-2 px-2.5 p-15 grid grid-cols-[100px_auto] gap-x-5 items-center h-48 shadow-lg rounded-xl hover:-translate-y-2 hover:duration-200 hover:ease-in"
+              }
+              onClick={(e) => handleProductClick(e, product)}
+            >
               <img
                 src={product.imageUrl}
                 alt="beverage"
@@ -47,13 +135,42 @@ function App() {
               />
               <div className={"text-base h-20 justify-start items-center grid"}>
                 <label>{product.nameKor}</label>
-                <label className='text-zinc-500'>{product.name}</label>
-                <label className={"font-bold"}>{product.price}원</label>
+                <label className="text-zinc-500">{product.name}</label>
+                <label className={"font-bold"}>
+                  {convertNumType(product.price)}
+                </label>
               </div>
             </li>
           );
         })}
       </ul>
+      <section>
+        <div className={"p-2.5"}>
+          <div className={"grid grid-cols-3 h-10 items-center text-lg"}>
+            <label>주문내역</label>
+            <label>수량</label>
+            <label>소계</label>
+          </div>
+          {orderList.map((item) => {
+            const product = products.find((product) => product.id === item.id);
+            return (
+              <div
+                key={item.id}
+                className={"grid grid-cols-3 h-10 items-center text-sm"}
+              >
+                {/*<p>{product.id}</p>*/}
+                <label>{product?.nameKor}</label>
+                <div>
+                  {/*<button>-</button>*/}
+                  <label>{item.count}</label>
+                  {/*<button>+</button>*/}
+                </div>
+                <label>{convertNumType(item.count * product?.price)} 원</label>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        '3x250': 'repeat(3, 250px)',
+        '3x300': 'repeat(3, 300px)',
+        '3x350': 'repeat(3, 350px)',
+        '3x400': 'repeat(3, 400px)',
+      }
+    },
   },
   plugins: [],
 }
